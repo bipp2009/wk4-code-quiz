cc25b9bce561e5f349f839fbea2f8d6308b2477c
Bipin <bipp2009@gmail.com>
2021-03-14 12:54:30 +0000

    variables created in js to get elements from html

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Get everything in here from the html
+var startScreen = document.getElementById("start-screen");
+var startbuttonquiz = document.getElementById("Start-button")
+var question = document.getElementById("question")
+var answer = document.getElementById("answers")
 
 //Create an array of objects which will contain our questions
 
